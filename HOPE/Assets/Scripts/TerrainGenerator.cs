06218a4f01bb8d5ff158c05bdaa9be2a64bb48e7
Benjamin BONTEMPS <benjamin.bontemps@telecom-paristech.fr>
2015-10-23 17:08:08 +0200

    Creating and deleting walls works

--- HOPE/Assets/Scripts/TerrainGenerator.cs
+++ HOPE/Assets/Scripts/TerrainGenerator.cs
@@ -19,14 +19,26 @@ public class TerrainGenerator : MonoBehaviour {
 	}
 
 	void Update() {
-		RaycastHit hit;
+
+		RaycastHit[] hits;
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if (Input.GetMouseButton (1)) {
-			if (Physics.Raycast(ray, out hit)) {
+		hits = Physics.RaycastAll (ray);
+
+		for (int i = 0; i < hits.Length; i++) {
+
+			RaycastHit hit = hits[i];
+
+			if (Input.GetMouseButton (0)) {
 				if (hit.transform.gameObject.tag == "Floor") {
 					GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 					cube.transform.position = new Vector3(hit.transform.position.x, 0, hit.transform.position.z);
 					cube.transform.localScale = new Vector3(1,3,1);
+				}
+			}
+
+			if (Input.GetMouseButton (1)) {
+				if (hit.transform.gameObject.tag != "Floor") {
+					Destroy (hit.transform.gameObject);
 				}
 			}
 		}
