99546e3cf2b7d0d2c3ebc999e7dd132fe0104c3f
Tanguy Courreges <tanguy.courreges@gmail.com>
2015-10-23 16:16:03 +0200

    basic laser interaction

---
+++ HOPE/Assets/Scripts/Laser.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laser : MonoBehaviour {
+
+	private LineRenderer line;
+	public GameObject origin, end;
+
+	private Vector3 x0, xi, xf;
+	RaycastHit hit;
+
+	void setOrigin(GameObject o) {origin = o;}
+	void setEnd(GameObject e) {end = e;}
+
+	// Use this for initialization
+	void Start () {
+		line=GetComponent<LineRenderer>();
+		x0=origin.transform.position;
+		xf=end.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		xi=xf;
+
+		if(Physics.Raycast(x0, xf-x0, out hit)) {
+			GameObject col=hit.collider.gameObject;
+			xi=col.transform.position;
+			if(col.tag == "Tower" ) {
+				//col = tour touchée par le laser
+
+				
+			}
+		}
+
+		line.SetPosition(0, x0);	
+		line.SetPosition(1, xi);
+	}
+}
---
+++ HOPE/Assets/Scripts/Tower.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	public bool emitsLaser = true;
+	public bool isPowered = false;
+
+	public GameObject laser;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//void setLaser(GameObject target)
+}
